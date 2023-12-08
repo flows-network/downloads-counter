bbbcada184cda4cd4784d1716bb2e9bce2567b33
jaykchen@gmail.com <jaykchen@gmail.com>
2023-12-08 17:42:38 -0500

    add route to query counts

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,87 @@ use serde_json::Value;
 use std::collections::HashMap;
 use webhook_flows::{
     create_endpoint, request_handler,
-    route::{get, options, route, RouteError, Router},
+    route::{get, route, RouteError, Router},
     send_response,
 };
 
-#[request_handler]
-async fn handle() {
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn on_deploy() {
+    dotenv().ok();
+    create_endpoint().await;
+}
+
+#[request_handler(GET)]
+async fn handler(
+    _headers: Vec<(String, String)>,
+    _subpath: String,
+    _qry: HashMap<String, Value>,
+    _body: Vec<u8>,
+) {
+    logger::init();
+
     let mut router = Router::new();
 
     router
-        .insert("/query/:count", vec![options(opt), get(query)])
+        .insert("/query/:count", vec![get(query)])
         .unwrap();
+
+
+    let urls_map = create_map().await;
+
+    let mut key = String::new();
+
+    match _qry.get("file") {
+        Some(m) => match serde_json::from_value::<String>(m.clone()) {
+            Ok(s) => {
+                if !urls_map.contains_key(&s) {
+                    log::error!("invalid file_name: {}", s);
+                    return;
+                } else {
+                    key = s;
+                }
+            }
+            Err(_e) => {
+                log::error!("failed to parse file_name: {}", _e);
+                return;
+            }
+        },
+        _ => {
+            log::error!("missing file_name");
+            return;
+        }
+    }
+
+    let download_url = match urls_map.get(&key) {
+        Some(m) => m,
+        None => {
+            log::error!("missing download_url for file: {}", key);
+            return;
+        }
+    };
+    let mut download_count = match store_flows::get(&key) {
+        Some(val) => match serde_json::from_value::<i32>(val) {
+            Ok(n) => n,
+            Err(_e) => {
+                log::error!("failed to parse download_count from store: {}", _e);
+                0
+            }
+        },
+        None => 0,
+    };
+    download_count += 1;
+    store_flows::set(&key, serde_json::json!(download_count), None);
+
+    log::info!("{} downloaed {} times", key, download_count);
+
+    send_response(
+        302, // HTTP status code for Found (Redirection)
+        vec![
+            ("Location".to_string(), download_url.to_string()), // Redirect URL in the Location header
+        ],
+        Vec::new(), // No body for a redirection response
+    );
 
     if let Err(e) = route(router).await {
         match e {
@@ -26,6 +96,7 @@ async fn handle() {
             }
         }
     }
+
 }
 
 async fn query(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, _body: Vec<u8>) {
@@ -64,13 +135,6 @@ async fn query(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, _bo
     );
 }
 
-#[no_mangle]
-#[tokio::main(flavor = "current_thread")]
-pub async fn on_deploy() {
-    dotenv().ok();
-    create_endpoint().await;
-}
-
 async fn create_map() -> HashMap<String, String> {
     let json_contents = include_str!("../URLS.json");
     let mut paths_list = Vec::<String>::new();
@@ -92,15 +156,8 @@ async fn create_map() -> HashMap<String, String> {
         .collect::<HashMap<String, String>>()
 }
 
-#[request_handler(GET, POST)]
-async fn handler(
-    _headers: Vec<(String, String)>,
-    _subpath: String,
-    _qry: HashMap<String, Value>,
-    _body: Vec<u8>,
-) {
-    logger::init();
 
+/* async fn track_and_redirect<F>(_qry: HashMap<String, Value>) -> Option<String> {
     let urls_map = create_map().await;
 
     let mut key = String::new();
@@ -110,30 +167,30 @@ async fn handler(
             Ok(s) => {
                 if !urls_map.contains_key(&s) {
                     log::error!("invalid file_name: {}", s);
-                    return;
+                    return None;
                 } else {
                     key = s;
                 }
             }
             Err(_e) => {
                 log::error!("failed to parse file_name: {}", _e);
-                return;
+                return None;
             }
         },
         _ => {
             log::error!("missing file_name");
-            return;
+            return None;
         }
     }
 
     let download_url = match urls_map.get(&key) {
-        Some(m) => m,
+        Some(m) => m.to_string(),
         None => {
             log::error!("missing download_url for file: {}", key);
-            return;
+            return None;
         }
     };
-    let mut download_count = match get(&key) {
+    let mut download_count = match store_flows::get(&key) {
         Some(val) => match serde_json::from_value::<i32>(val) {
             Ok(n) => n,
             Err(_e) => {
@@ -148,11 +205,5 @@ async fn handler(
 
     log::info!("{} downloaed {} times", key, download_count);
 
-    send_response(
-        302, // HTTP status code for Found (Redirection)
-        vec![
-            ("Location".to_string(), download_url.to_string()), // Redirect URL in the Location header
-        ],
-        Vec::new(), // No body for a redirection response
-    );
-}
+    Some(download_url)
+} */
